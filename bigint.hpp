246622166f1e1b9eb4b54f0f7f549bff6ec6a420
Alexandra Cong <xandrac@Alexandras-MacBook-Air.local>
2024-12-19 21:44:14 -0500

    add subtract, multiply and helper

--- bigint.hpp
+++ bigint.hpp
@@ -42,13 +42,16 @@ public:
     bigint operator++();               
     bigint operator--(); 
 
-    //helper functions
-    bigint negate(const bigint &);             
+         
 private: 
     vector<uint8_t> digits;
     bool is_negative;
 
 
+    //helper functions
+    bigint negate(const bigint &);  
+    bigint add(const bigint&, const bigint&);  
+    bigint subtract(const bigint&, const bigint&);
 };
 
 
@@ -107,46 +110,86 @@ bigint bigint::negate(const bigint& num)
     return result;
 }
 
-
-bigint bigint::operator+(const bigint& other) 
-{   
+bigint bigint::add(const bigint& num1, const bigint& num2)
+{
     bigint result;
-    if(is_negative ==other.is_negative) 
-    {
-        
-        uint8_t carry = 0;
+    uint8_t carry = 0;
 
-        size_t size1 = digits.size();
-        size_t size2 = other.digits.size();
-        size_t size = max(size1, size2);  //take the larger size for for loop 
+    size_t size1 = num1.digits.size();
+    size_t size2 = num2.digits.size();
+    size_t size = max(size1, size2);  //take the larger size for for loop 
 
-        for(size_t i = 0; i < size; i++) {
+    for(size_t i = 0; i < size; i++) {
             //if i exceeds the size of digits, set it to 0
-            uint8_t digit1 = (i < size1) ? digits[i] : 0;       
-            uint8_t digit2 = (i < size2) ? other.digits[i] : 0;
+            uint8_t digit1 = (i < size1) ? num1.digits[i] : 0;       
+            uint8_t digit2 = (i < size2) ? num2.digits[i] : 0;
 
             uint8_t sum = digit1 + digit2 + carry; 
             result.digits.push_back(sum % 10);
             //prepare carry for next addition
             carry = sum / 10;
+    }
+    //push remaining carry to result
+    if(carry > 0) {
+        result.digits.push_back(carry);
+    }
+
+    return result;
+}
+
+bigint bigint::subtract(const bigint& num1, const bigint& num2)
+{
+    bigint result;
+    uint8_t borrow = 0;
+
+    size_t size1 = num1.digits.size();
+    size_t size2 = num2.digits.size();
+    size_t max_size = max(size1, size2); 
+
+    for (size_t i = 0; i < max_size; i++) {
+        uint8_t digit1 = (i < size1) ? num1.digits[i] : 0;       
+        uint8_t digit2 = (i < size2) ? num2.digits[i] : 0;
+
+        uint8_t diff = digit1 - digit2 - borrow;
+
+        if (diff < 0) {
+            diff += 10;
+            borrow = 1;
+        } else {
+            borrow = 0;
         }
 
-        //push remaining carry to result
-        if(carry > 0) {
-            result.digits.push_back(carry);
-        }
+        result.digits.push_back(diff);
+    }
+
+    // Remove leading zeros
+    while (result.digits.size() > 1 && result.digits.back() == 0) {
+        result.digits.pop_back();
+    }
+
+    return result;
+}
+
+// operator logic 
+bigint bigint::operator+(const bigint& other) 
+{   
+    bigint result;
+    if(is_negative ==other.is_negative) 
+    {
+        result = add(*this, other);
 
         result.is_negative = is_negative;
         return result;
     } else {
-        if(is_negative)
+        // if signs are different, perform subtraction
+        if (is_negative) 
         {
-            bigint positive_this = *this;
-            positive_this.is_negative = false;
-            return other-positive_this;
-        } else{
-            bigint positive_other = other;
-            positive_other.is_negative = false;
+            bigint positive_this = negate(*this); 
+            return other - positive_this;
+        } 
+        else 
+        {
+            bigint positive_other = negate(other); 
             return *this - positive_other;
         }
     }
@@ -168,14 +211,87 @@ bigint bigint::operator-(const bigint& other)
         if(*this < other) 
         {
             //use other to subtract this, set is_negative to true
+            result = subtract(other, *this); 
+            result.is_negative = true;
+
         } else {
              //use this to subtract other, set is_negative to false
+            result = subtract(*this, other); 
+            result.is_negative = false;
         }
     } else {     
-        //different sign, addition
+        result = add(*this, other);
+        result.is_negative = is_negative;
+    
     }
+    return result;
+    
 }
 
+bigint bigint::operator-=(const bigint& other)
+{
+    *this = *this - other;
+    return *this;
+}
+
+
+
+bigint bigint::operator*(const bigint& other)
+{
+    bigint result = bigint(0);
+
+    // If one of the numbers is zero, return zero
+    if (digits.size() == 1 && digits[0] == 0 || other.digits.size() == 1 && other.digits[0] == 0)
+    {
+        return result;
+    }
+
+    for (size_t i = 0; i < digits.size(); i++) 
+    {
+        bigint temp; // Temporary bigint to store intermediate results
+        uint8_t carry = 0;
+
+        // Shift the digits in temp based on the current position in the first number
+        for (size_t j = 0; j < i; j++) 
+        {
+            temp.digits.push_back(0); 
+        }
+        
+        for (size_t j = 0; j < other.digits.size(); j++) 
+        {
+            uint16_t product = digits[i] * other.digits[j] + carry; 
+            temp.digits.push_back(product % 10); 
+            carry = product / 10; 
+        }
+
+        // for any carry left, add it to temp
+        if (carry > 0) 
+        {
+            temp.digits.push_back(carry);
+        }
+
+        result = add(result, temp);
+    }
+
+    
+    result.is_negative = is_negative ^ other.is_negative;
+
+    return result;
+}
+
+bigint bigint::operator*=(const bigint& other)
+{
+    *this = *this * other;
+    return *this;
+}
+
+
+bigint bigint::operator-()
+{
+    bigint result = *this;
+    result.is_negative = !is_negative;
+    return result;
+}
 
 
 
